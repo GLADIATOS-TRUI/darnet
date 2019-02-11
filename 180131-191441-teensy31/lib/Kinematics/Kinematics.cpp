d64af5c05dd1a2ad23545a9b5a7b876651e829ed
Yo Panjikhrisna <36294277+yopanji@users.noreply.github.com>
2019-02-11 16:55:11 +0700

    Update Kinematics.cpp
    Update untuk parameter Darnet

--- 180131-191441-teensy31/lib/Kinematics/Kinematics.cpp
+++ 180131-191441-teensy31/lib/Kinematics/Kinematics.cpp
@@ -13,11 +13,11 @@ using namespace Robot;
 
 const double Kinematics::CAMERA_DISTANCE = 45.0; //mm
 const double Kinematics::EYE_TILT_OFFSET_ANGLE = 40.0;//45.0; //degree
-const double Kinematics::LEG_SIDE_OFFSET = 37.0;//37.5; //mm
-const double Kinematics::THIGH_LENGTH = 93.0; //mm
-const double Kinematics::CALF_LENGTH = 95.0; //mm
-const double Kinematics::ANKLE_LENGTH = 55.0; //mm
-const double Kinematics::LEG_LENGTH = 243.0; //mm (THIGH_LENGTH + CALF_LENGTH + ANKLE_LENGTH)
+const double Kinematics::LEG_SIDE_OFFSET = 48.75;//48.7; //mm
+const double Kinematics::THIGH_LENGTH = 124.0; //mm
+const double Kinematics::CALF_LENGTH = 120.0; //mm
+const double Kinematics::ANKLE_LENGTH = 60.0; //mm
+const double Kinematics::LEG_LENGTH = 304.0; //mm (THIGH_LENGTH + CALF_LENGTH + ANKLE_LENGTH)
 
 Kinematics* Kinematics::m_UniqueInstance = new Kinematics();
 
